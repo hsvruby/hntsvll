f0c713a179822498e64a584a669b14334481b158
Bryan Powell <bryan@metabahn.com>
2011-02-15 16:46:35 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hntsvll::Application.config.secret_token = '026c4abcadbe9e26b06046992de530e6dd2c06bdc60ad5d83a0e0418c71dafb13f0c6ca02ee4f38a38b2bab05d53c9258cfae0bcb758da6d8ec5de0b4055dc04'
