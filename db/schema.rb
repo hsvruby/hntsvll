8ad2e7442eef4304ec7503052adfd20860cde2a3
PJ Davis <pj.davis@gmail.com>
2011-02-23 16:58:45 -0600

    Added page_url for the user to put in a url for their profile to be linked to

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110223151817) do
+ActiveRecord::Schema.define(:version => 20110223215240) do
 
   create_table "accounts", :force => true do |t|
     t.string   "first_name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110223151817) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.string   "page_url"
   end
 
   create_table "accounts_categories", :id => false, :force => true do |t|
