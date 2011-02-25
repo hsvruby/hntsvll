6337c67226a75a1e27bbb0d9b6509626750cb652
Andy Lindeman <andy@highgroove.com>
2011-02-24 21:08:15 -0600

    Complete TODO items: Users should be listed on the home page in random order. The order should be saved to a cookie so order is retained for future visits, Should be able to sort users by first name, last name, and date added. There should also be an option to revert back to the original random order

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,2 +1,6 @@
 module AccountsHelper
+  ORDER_BY_OPTIONS = {'First Name' => 'first_name',
+                      'Last Name' => 'last_name',
+                      'Date Added' => 'created_at',
+                      'Random' => 'random'}
 end
