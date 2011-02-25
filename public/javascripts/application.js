65341dd86ad58225e4b6b11be9d8dad8af6e3563
Andy Lindeman <andy@highgroove.com>
2011-02-24 22:34:07 -0600

    Gracefully degrade when JavaScript is disabled

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,5 @@
-function update_accounts_list(e) {
-  $.get(location.href, { search: $('#search_text_field').val(), order_by: $('#order_by_select').val() }, null, 'script');
-}
 
 $(function() {
-  $('#order_by_select').change(update_accounts_list);
-  $('#search_text_field').keyup(update_accounts_list);
+  $('#order_by_select').change(function() { $('#filter_form').submit(); });
+  $('#search_text_field').keyup(function() { $('#filter_form').submit(); });
 });
