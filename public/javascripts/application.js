51c3c4d1d4f83c7e4a9e0216572ab6d6542eda0e
Andy Lindeman <andy@highgroove.com>
2011-02-25 23:47:08 -0600

    OMG fade effect.  Comment if you think this is a bit much?

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,8 @@
 $(function() {
+  $('#filter_form').bind('ajax:beforeSend', function() {
+    $('#accounts_list').fadeOut('fast');
+  });
+
   $('#order_by_select').change(function() { $('#filter_form').submit(); });
   $('#search_text_field').keyup(function() { $('#filter_form').submit(); });
 
