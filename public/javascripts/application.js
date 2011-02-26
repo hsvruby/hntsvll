ceb834e7cb5cda46e322fa15dcd40208453fe596
Andy Lindeman <andy@highgroove.com>
2011-02-26 00:01:45 -0600

    Do not fire a search until the user has settled in the search field for ~1 second

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,17 @@ $(function() {
   });
 
   $('#order_by_select').change(function() { $('#filter_form').submit(); });
-  $('#search_text_field').keyup(function() { $('#filter_form').submit(); });
+
+  $('#search_text_field').keyup(function() {
+    // Do not fire a search until the user has settled in the field for ~1 second
+    clearTimeout($(this).data('search_delay'));
+    $(this).data('search_delay', setTimeout(function() {
+      if ($(this).data('search_value') != $(this).val()) {
+        $(this).data('search_value', $(this).val());
+        $('#filter_form').submit();
+      }
+    }, 700));
+  });
 
   $('#search_text_field').autocomplete({
     source: '/accounts/autocomplete',
