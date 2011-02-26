f22abf3f8fe7084f7712e0a0841731387230bb21
Andy Lindeman <andy@highgroove.com>
2011-02-26 00:10:37 -0600

    Burned by closures: fix the AJAX form submission

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,14 +6,16 @@ $(function() {
   $('#order_by_select').change(function() { $('#filter_form').submit(); });
 
   $('#search_text_field').keyup(function() {
-    // Do not fire a search until the user has settled in the field for ~1 second
-    clearTimeout($(this).data('search_delay'));
-    $(this).data('search_delay', setTimeout(function() {
-      if ($(this).data('search_value') != $(this).val()) {
-        $(this).data('search_value', $(this).val());
+    var search_field = $(this);
+
+    // Do not fire a search until the user has settled in the field for 1/2 second
+    clearTimeout(search_field.data('search_delay'));
+    search_field.data('search_delay', setTimeout(function() {
+      if (search_field.data('search_value') != search_field.val()) {
+        search_field.data('search_value', search_field.val());
         $('#filter_form').submit();
       }
-    }, 700));
+    }, 500));
   });
 
   $('#search_text_field').autocomplete({
